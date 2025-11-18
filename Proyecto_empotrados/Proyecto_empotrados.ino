c5a990d2982d8e4a69712a41875efef7036cb19c
Eliasmtain11 <83424623+Eliasmtain11@users.noreply.github.com>
2025-11-19 00:40:38 +0100

    Update Proyecto_empotrados.ino

--- Proyecto_empotrados/Proyecto_empotrados.ino
+++ Proyecto_empotrados/Proyecto_empotrados.ino
@@ -16,34 +16,62 @@
 #define LED_INTERVAL 500            
 #define SERVICE_DURATION 5000       
 #define DHT_INTERVAL 1000          
+#define TAKE_DRINK_TIME 3000
 
-void INICIO();
-void SERVICIO();
-long leerDistancia();
-bool temporizador(unsigned long &ultimo, unsigned long intervalo);
-void parpadear_led();
-void dhtCallback();
 
 LiquidCrystal lcd(9,8,7,6,5,4);
 DHT dht(DHT11PIN, DHTTYPE);
 
 Thread dhtThread;
 
+byte euro[8] = {
+  B00110,
+  B01001,
+  B11100,
+  B01000,
+  B11100,
+  B01001,
+  B00110,
+  B00000
+};
+
+
 byte state = 0;
 bool person_detected = false;
 bool menu_service = false;
 
-unsigned long lastLedTime = 0;
+unsigned long last_led_time = 0;
 unsigned long service_start_time = 0;
+unsigned long temp1 = 0;
+unsigned long temp2 = 0;
+unsigned long temp3 = 0;
 
 bool led_1_state = false;
+bool done = false;
+bool coffee_selected = false;
+
 byte iteracion = 0;
+int last_state = 0;
+int lectura = 0;
+int i = 0;
+int brillo_led = 0;
+int brightness = 0;
+int time = 0;
+int step = 0;
 
 char* cafes[] = {"Cafe Solo", "Cafe Cortado", "Cafe Doble", "Cafe Premium", "Chocolate"};
 float precios[] = {1.00, 1.10, 1.25, 1.50, 2.00};
 
+byte len = sizeof(cafes) / sizeof(cafes[0]);
+
+enum AXIS {
+ X_AXIS = 1,
+ Y_AXIS = 2
+};
+
 void setup() {
   lcd.begin(16,2);
+  lcd.createChar(0, euro);  
   Serial.begin(9600);
   dht.begin();
 
@@ -109,8 +137,57 @@ void SERVICIO() {
     }
 
   } else {
-    if (menu_service) {
+    if (menu_service && person_detected) {
+      byte joystick_button_state = digitalRead(BUTTON_JOYSTICK);
 
+      if (last_state != lectura) {
+        i += lectura;
+        last_state = lectura;
+      }
+      lectura = joystick(Y_AXIS);
+      Serial.println(i);
+      if (i < 0){
+        i = len - 1;
+      }
+      else if (i > len - 1){
+        i = 0;
+      }
+      lcd.setCursor(0, 0);
+      lcd.print(cafes[i]);
+      lcd.print("                ");
+      lcd.setCursor(11, 1);
+      lcd.print((precios[i]));
+      lcd.write(byte(0));
+
+      if(joystick_button_state == LOW){
+        coffee_selected = true;
+        time = random(4,8);
+        menu_service = false;
+        done = false;
+        step = 1024 / time;
+        lcd.clear();
+      }
+    }
+    if(coffee_selected){
+      done = temporizador(temp1,time);
+      if(temporizador(temp3,step)){
+        analogWrite(LED_GREEN_PIN, brightness+=step);
+      }
+      if(done){
+        if(temporizador(temp2,TAKE_DRINK_TIME)){
+          person_detected = false;
+        }
+        lcd.setCursor(6,0);
+        lcd.print("RETIRE   ");
+        lcd.setCursor(6,1);
+        lcd.print("BEBIDA         ");
+      }
+
+      lcd.setCursor(3,0);
+      lcd.print("Preparando   ");
+      lcd.setCursor(8,1);
+      lcd.print("cafe         ");
+      
     }
   }
 }
@@ -136,11 +213,16 @@ long leerDistancia() {
 }
 
 void parpadear_led() {
-  if (temporizador(lastLedTime, LED_INTERVAL)) {
+  if (temporizador(last_led_time, LED_INTERVAL)) {
     led_1_state = !led_1_state;
     iteracion++;
   }
-  digitalWrite(LED_RED_PIN, led_1_state ? HIGH : LOW);
+  if (led_1_state) {
+      digitalWrite(LED_RED_PIN, HIGH);
+  } else {
+      digitalWrite(LED_RED_PIN, LOW);
+  }
+
 }
 
 void dhtCallback() {
@@ -181,32 +263,27 @@ bool temporizador(unsigned long &last, unsigned long interval) {
   return false;
 }
 
-byte joystick(byte value_ant, byte axis) {
+int joystick(byte axis) {
   int value = 0;
-  byte n = 0;
+  int n = 0;
 
   if(axis == 1){
-    value = analogRead(Y_JOYSTICK);
-  }
-  else if(axis == 2) {
     value = analogRead(X_JOYSTICK);
   }
+  else if(axis == 2) {
+    value = analogRead(Y_JOYSTICK);
+  }
 
-  if(value <= 100) {
+  if(value <= 400) {
     n = -1;
   }
 
-  else if(value > 100 && value <= 900) {
-    n = 0
+  else if(value > 400 && value <= 900) {
+    n = 0;
   }
 
   else if(value > 900) {
     n = 1;
   }
-
-  if (n != 0 && n == value_ant) {
-    return 0;
-  }
-
   return n;
 }
