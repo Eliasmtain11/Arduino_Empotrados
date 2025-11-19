fb37fd3ba47b30fd08fc6cc41612ae83c0af3d6a
Eliasmtain11 <83424623+Eliasmtain11@users.noreply.github.com>
2025-11-19 21:43:15 +0100

    Refactor coffee machine control logic
    Refactor coffee machine code to improve structure and readability, including state management and function names.

--- Proyecto_empotrados/Proyecto_empotrados.ino
+++ Proyecto_empotrados/Proyecto_empotrados.ino
@@ -12,17 +12,32 @@
 #define LED_GREEN_PIN 10
 #define BUTTON_JOYSTICK 12
 #define BUTTON 2
-
+#define SECONDS_TO_MILI 1000UL
 #define LED_INTERVAL 500            
 #define SERVICE_DURATION 5000       
 #define DHT_INTERVAL 1000          
 #define TAKE_DRINK_TIME 3000
-
-
 LiquidCrystal lcd(9,8,7,6,5,4);
 DHT dht(DHT11PIN, DHTTYPE);
 
 Thread dhtThread;
+
+unsigned long last_led1_time = 0;
+unsigned long timer = 0;
+unsigned long inicio = 0;
+unsigned long fin = 0;
+
+enum STATES {
+  INIT = 0,
+  WAIT_FOR_CLIENT,
+  MENU_COFFEE,
+  COFFEE_SELECTED,
+  COFFEE_FINISHED,
+  ADMIN,
+
+};
+
+byte state = INIT;
 
 byte euro[8] = {
   B00110,
@@ -35,39 +50,32 @@ byte euro[8] = {
   B00000
 };
 
+char* coffees[] = {"Cafe Solo", "Cafe Cortado", "Cafe Doble", "Cafe Premium", "Chocolate"};
+float prices[] = {1.00, 1.10, 1.25, 1.50, 2.00};
 
-byte state = 0;
+
 bool person_detected = false;
-bool menu_service = false;
-
-unsigned long last_led_time = 0;
-unsigned long service_start_time = 0;
-unsigned long temp1 = 0;
-unsigned long temp2 = 0;
-unsigned long temp3 = 0;
-
 bool led_1_state = false;
-bool done = false;
-bool coffee_selected = false;
+volatile bool change = false;   
 
-byte iteracion = 0;
+
 int last_state = 0;
-int lectura = 0;
+int lecture = 0;
 int i = 0;
-int brillo_led = 0;
-int brightness = 0;
-int time = 0;
-int step = 0;
 
-char* cafes[] = {"Cafe Solo", "Cafe Cortado", "Cafe Doble", "Cafe Premium", "Chocolate"};
-float precios[] = {1.00, 1.10, 1.25, 1.50, 2.00};
-
-byte len = sizeof(cafes) / sizeof(cafes[0]);
+byte len = sizeof(coffees) / sizeof(coffees[0]);
+byte brightness;
+byte time = 0;
+byte iteracion = 0;
 
 enum AXIS {
  X_AXIS = 1,
  Y_AXIS = 2
 };
+
+void buttonISR() {
+  change = true;   
+}
 
 void setup() {
   lcd.begin(16,2);
@@ -89,63 +97,72 @@ void setup() {
   dhtThread.onRun(dhtCallback); 
   dhtThread.setInterval(DHT_INTERVAL);  
   dhtThread.enabled = false;            
+  randomSeed(analogRead(A0)); 
+  attachInterrupt(digitalPinToInterrupt(BUTTON), buttonISR, CHANGE);
 
-  state = 0;
 }
- // 67
+
 void loop() {
+  button_timing();
   if (dhtThread.shouldRun()) {
     dhtThread.run();
   }
-
-  if (state == 0) {
-    INICIO();
-  } else if (state == 1) {
-    SERVICIO();
+  if (state == INIT) {
+    start();
+  }
+  else if(state == WAIT_FOR_CLIENT) {
+    wait_and_temp_hum();
+  }
+  else if (state == MENU_COFFEE) {
+    coffee_menu();
+  }
+  else if (state == COFFEE_SELECTED) {
+    preprare_coffee();
+  }
+  else if (state == COFFEE_FINISHED) {
+    extract_coffee();
   }
 }
 
-void INICIO() {
+void start() {
   lcd.setCursor(0,0);
   lcd.print("CARGANDO...      ");
   parpadear_led();
 
   if (iteracion >= 6) {
-    state = 1;
+    state = WAIT_FOR_CLIENT;
     person_detected = false;
-    menu_service = false;
     lcd.clear();
   }
 }
 
-void SERVICIO() {
+void wait_and_temp_hum() {
   if (!person_detected) {
+    digitalWrite(LED_GREEN_PIN,LOW);
     long dist = leerDistancia();
-
+    
     lcd.setCursor(0,0);
     lcd.print("    ESPERANDO    ");
     lcd.setCursor(0,1);
     lcd.print("     CLIENTE     ");
 
     if (dist > 0 && dist < 100) {
-      person_detected = true;
-      menu_service = false;
-      service_start_time = millis();  
-
-      dhtThread.enabled = true;            
-      lcd.clear();
+    timer = millis();  
+    person_detected = true;
+    dhtThread.enabled = true;
+    i = 0;            
+    lcd.clear();
     }
+  }
+}
 
-  } else {
-    if (menu_service && person_detected) {
-      byte joystick_button_state = digitalRead(BUTTON_JOYSTICK);
-
-      if (last_state != lectura) {
-        i += lectura;
-        last_state = lectura;
+void coffee_menu() {
+  byte joystick_button_state = digitalRead(BUTTON_JOYSTICK);
+  lecture = joystick(Y_AXIS);
+  if (last_state != lecture) {
+        i += lecture;
+        last_state = lecture;
       }
-      lectura = joystick(Y_AXIS);
-      Serial.println(i);
       if (i < 0){
         i = len - 1;
       }
@@ -153,87 +170,99 @@ void SERVICIO() {
         i = 0;
       }
       lcd.setCursor(0, 0);
-      lcd.print(cafes[i]);
+      lcd.print(coffees[i]);
       lcd.print("                ");
       lcd.setCursor(11, 1);
-      lcd.print((precios[i]));
+      lcd.print((prices[i]));
       lcd.write(byte(0));
 
       if(joystick_button_state == LOW){
-        coffee_selected = true;
+
+        state = COFFEE_SELECTED;
+
         time = random(4,8);
-        menu_service = false;
-        done = false;
-        step = 1024 / time;
+        brightness = 0;
+        analogWrite(LED_GREEN_PIN, brightness);
+
+        timer = millis();
+        Serial.println(time);
         lcd.clear();
       }
-    }
-    if(coffee_selected){
-      done = temporizador(temp1,time);
-      if(temporizador(temp3,step)){
-        analogWrite(LED_GREEN_PIN, brightness+=step);
-      }
-      if(done){
-        if(temporizador(temp2,TAKE_DRINK_TIME)){
-          person_detected = false;
-        }
-        lcd.setCursor(6,0);
-        lcd.print("RETIRE   ");
-        lcd.setCursor(6,1);
-        lcd.print("BEBIDA         ");
-      }
-
-      lcd.setCursor(3,0);
-      lcd.print("Preparando   ");
-      lcd.setCursor(8,1);
-      lcd.print("cafe         ");
-      
-    }
-  }
 }
 
-long leerDistancia() {
-  long duracion;
-  float distancia;
+void preprare_coffee() {
 
-  digitalWrite(TRIG_PIN, LOW);
-  delayMicroseconds(2);
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG_PIN, LOW);
+  unsigned long total_ms = (unsigned long)time * SECONDS_TO_MILI; 
+  unsigned long elapsed  = millis() - timer;                      
 
-  duracion = pulseIn(ECHO_PIN, HIGH, 30000); // timeout de 30 ms
+  if (elapsed < total_ms) {
 
-  if (duracion == 0) {
-    return -1;
-  }
-  distancia = duracion * 0.0343f / 2.0f;
+    brightness = (elapsed * 255) / total_ms;
+    if (brightness > 255){
+      brightness = 255;
+    }
+    analogWrite(LED_GREEN_PIN, brightness);
 
-  return distancia;  
-}
+    lcd.setCursor(3,0);
+    lcd.print("Preparando   ");
+    lcd.setCursor(3,1);
+    lcd.print("cafe...         ");
 
-void parpadear_led() {
-  if (temporizador(last_led_time, LED_INTERVAL)) {
-    led_1_state = !led_1_state;
-    iteracion++;
-  }
-  if (led_1_state) {
-      digitalWrite(LED_RED_PIN, HIGH);
   } else {
-      digitalWrite(LED_RED_PIN, LOW);
+    timer = millis();
+    lcd.clear();
+    state = COFFEE_FINISHED;
   }
+}
 
+void extract_coffee() {
+  if(!(temporizador(timer,TAKE_DRINK_TIME))) {
+    lcd.setCursor(5,0);
+    lcd.print("RETIRE   ");
+    lcd.setCursor(5,1);
+    lcd.print("BEBIDA         ");
+ }
+  else {
+    lcd.clear();
+    person_detected = false;
+    state = WAIT_FOR_CLIENT;
+  }
+}
+
+void menu_admin() {
+  
+}
+
+void button_timing(){
+  if (change) {      
+    change = false; // limpiar flag
+    unsigned long time_pressed = 0;
+    int val = digitalRead(BUTTON);
+
+    if (val == LOW) {
+      inicio = millis();
+    } else {
+      fin = millis();     // botón suelto
+      time_pressed = fin - inicio;
+    }
+    
+    if(time_pressed > 2000 && time_pressed < 3000){
+      state == WAIT_FOR_CLIENT;
+    }
+    else if(time_pressed > 5000) {
+      state == ADMIN;
+    }
+  }
 }
 
 void dhtCallback() {
-  if (!person_detected || menu_service) {
+  if (!person_detected) {
     dhtThread.enabled = false;
     return;
   }
-
-  unsigned long now = millis();
-  if (now - service_start_time >= SERVICE_DURATION) {
-    menu_service = true;
+  
+  if (temporizador(timer, SERVICE_DURATION)) {
+    state = MENU_COFFEE;
     dhtThread.enabled = false;
     lcd.clear();
     return;
@@ -254,6 +283,19 @@ void dhtCallback() {
 
 }
 
+void parpadear_led() {
+  if (temporizador(last_led1_time, LED_INTERVAL)) {
+    led_1_state = !led_1_state;
+    iteracion++;
+  }
+  if (led_1_state) {
+      digitalWrite(LED_RED_PIN, HIGH);
+  } else {
+      digitalWrite(LED_RED_PIN, LOW);
+  }
+
+}
+
 bool temporizador(unsigned long &last, unsigned long interval) {
   unsigned long now = millis();
   if (now - last >= interval) {
@@ -261,6 +303,26 @@ bool temporizador(unsigned long &last, unsigned long interval) {
     return true;
   }
   return false;
+}
+
+long leerDistancia() {
+  long duracion;
+  float distancia;
+
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  duracion = pulseIn(ECHO_PIN, HIGH, 30000); // timeout de 30 ms
+
+  if (duracion == 0) {
+    return -1;
+  }
+  distancia = duracion * 0.0343f / 2.0f;
+
+  return distancia;  
 }
 
 int joystick(byte axis) {
